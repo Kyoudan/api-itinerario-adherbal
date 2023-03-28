1bc5124591dd1b7e2d51172986b3b9f4c89a0c82
Kyoudan <fogoste@gmail.com>
2023-03-28 09:00:25 -0300

    add: login route created

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import ActiveAccountController from "../controllers/Email/ActiveAccount.controller";
+import LoginUserController from "../controllers/Login/LoginUser.controller";
 import CreateUserController from "../controllers/User/CreateUser.controller";
 const routes = Router();
 
 routes.post("/users", CreateUserController.handle);
+routes.post("/login", LoginUserController.handle);
 routes.get("/activateAccount", ActiveAccountController.handle);
 
 export default routes;
