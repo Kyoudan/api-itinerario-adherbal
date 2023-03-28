0cc430b43baab9cbc1112bb6077d64484d24e0e0
Kyoudan <fogoste@gmail.com>
2023-03-28 09:12:35 -0300

    add: route to fetch user data

---
+++ src/controllers/User/FindAllUsers.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import prismaClient from "../../database/prismaClient";
+
+class FindAllUsersController {
+  async handle(req: Request, res: Response) {
+    let queryInit = req.query.init as string;
+    let queryLimit = req.query.limit as string;
+
+    !queryInit ? (queryInit = "0") : undefined;
+    !queryLimit ? (queryLimit = "100000") : undefined;
+
+    const init = parseInt(queryInit);
+    const limit = parseInt(queryLimit);
+
+    try {
+      const result = await prismaClient.users.findMany({
+        skip: init,
+        take: limit,
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+      res.status(200).json(result);
+    } catch {
+      res.status(500).json({ message: "Erro ao buscar os dados" });
+    }
+  }
+}
+
+export default new FindAllUsersController();
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,8 +2,10 @@ import { Router } from "express";
 import ActiveAccountController from "../controllers/Email/ActiveAccount.controller";
 import LoginUserController from "../controllers/Login/LoginUser.controller";
 import CreateUserController from "../controllers/User/CreateUser.controller";
+import FindAllUsersController from "../controllers/User/FindAllUsers.controller";
 const routes = Router();
 
+routes.get("/users", FindAllUsersController.handle);
 routes.post("/users", CreateUserController.handle);
 routes.post("/login", LoginUserController.handle);
 routes.get("/activateAccount", ActiveAccountController.handle);
