15d51d6142d86186ddeaff9d78384682f8ccf5f4
Kyoudan <fogoste@gmail.com>
2023-03-28 09:48:35 -0300

    config: applying eslint

--- src/nodemailer/nodemailer.config.ts
+++ src/nodemailer/nodemailer.config.ts
@@ -1,18 +1,18 @@
-import * as nodemailer from 'nodemailer';
-import * as dotenv from 'dotenv';
+import * as nodemailer from "nodemailer";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 const user: string = process.env.NODEMAILER_USER as string;
 const password: string = process.env.NODEMAILER_PASS as string;
 
 const sendEmail = nodemailer.createTransport({
-	host: 'smtp.gmail.com',
-	port: 465,
-	secure: true,
-	auth: {
-		user: `${user}`,
-		pass: `${password}`,
-	},
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: `${user}`,
+    pass: `${password}`,
+  },
 });
 
 export default sendEmail;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import { config } from 'dotenv';
-import app from './app';
+import { config } from "dotenv";
+import app from "./app";
 config();
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
-	console.log(`🔥 back-end rodando na porta: http://localhost:${port}`)
+  console.log(`🔥 back-end rodando na porta: http://localhost:${port}`)
 );
