da9e7182beaf04cda97e17c197f1496a1298a895
Kyoudan <fogoste@gmail.com>
2023-03-28 09:53:52 -0300

    add: route to fetch a user

---
+++ src/controllers/User/FindOneUser.controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import prismaClient from "../../database/prismaClient";
+
+class FindOneUserController {
+  async handle(req: Request, res: Response) {
+    try {
+      const queryId = req.params.id;
+
+      if (!queryId) return res.status(400).json({ message: "Id invalido!!" });
+      const id = parseInt(queryId);
+      const result = prismaClient.users.findUnique({
+        where: {
+          id: id,
+        },
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+      res.status(200).json(result);
+    } catch {
+      res.status(500).json({ message: "Erro ao buscar os dados" });
+    }
+  }
+}
+
+export default new FindOneUserController();
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,9 +3,11 @@ import ActiveAccountController from "../controllers/Email/ActiveAccount.controll
 import LoginUserController from "../controllers/Login/LoginUser.controller";
 import CreateUserController from "../controllers/User/CreateUser.controller";
 import FindAllUsersController from "../controllers/User/FindAllUsers.controller";
+import FindOneUserController from "../controllers/User/FindOneUser.controller";
 const routes = Router();
 
 routes.get("/users", FindAllUsersController.handle);
+routes.get("/users/:id", FindOneUserController.handle);
 routes.post("/users", CreateUserController.handle);
 routes.post("/login", LoginUserController.handle);
 routes.get("/activateAccount", ActiveAccountController.handle);
