10dabaa99bc7ece6741e7cae463ebf23cb89f849
Kyoudan <fogoste@gmail.com>
2023-03-29 08:57:50 -0300

    add: route to fetch all posts

---
+++ src/controllers/Post/FindAllPosts.controller.ts
@@ -0,0 +1,48 @@
+import { Response } from "express";
+import { Request } from "express";
+import prismaClient from "../../database/prismaClient";
+
+class FindAllPostCategoriesController {
+  async handle(req: Request, res: Response) {
+    let queryInit = req.query.init as string;
+    let queryLimit = req.query.limit as string;
+
+    !queryInit ? (queryInit = "0") : undefined;
+    !queryLimit ? (queryLimit = "100000") : undefined;
+
+    const init = parseInt(queryInit);
+    const limit = parseInt(queryLimit);
+
+    try {
+      const result = await prismaClient.posts.findMany({
+        skip: init,
+        take: limit,
+        select: {
+          id: true,
+          name: true,
+          color: true,
+          slug: true,
+          createdAt: true,
+          category: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+          users: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+            },
+          },
+        },
+      });
+      res.status(200).json(result);
+    } catch {
+      res.status(500).json({ message: "Erro ao buscar os dados" });
+    }
+  }
+}
+
+export default new FindAllPostCategoriesController();
--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import CreatePostController from "../controllers/Post/CreatePost.controller";
+import FindAllPostsController from "../controllers/Post/FindAllPosts.controller";
 import AuthUser from "../middleware/AuthUser";
 const routes = Router();
 
+routes.get("/post", FindAllPostsController.handle);
 routes.post("/post", AuthUser, CreatePostController.handle);
 
 export default routes;
