53f7d27e013e8b3dd73d85f11ffb58c9db14a866
Kyoudan <fogoste@gmail.com>
2023-03-29 11:35:44 -0300

    add:route to update only one type of post contents

---
+++ src/controllers/PostContentType/UpdatePostContentType.controller.ts
@@ -0,0 +1,34 @@
+import { Response } from "express";
+import { Request } from "express";
+import prismaClient from "../../database/prismaClient";
+
+class UpdatePostCategoriesController {
+  async handle(req: Request, res: Response) {
+    try {
+      const { name } = req.body;
+      const queryId = req.params.id;
+
+      if (!name) return res.status(400).json({ message: "Nome invalido!!" });
+      if (!queryId) return res.status(400).json({ message: "id invalido!!" });
+      const id = parseInt(queryId);
+      if (isNaN(id)) return res.status(400).json({ message: "id is NaN" });
+      const result = await prismaClient.postContentType.update({
+        data: {
+          name,
+        },
+        where: {
+          id,
+        },
+      });
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res.status(400).json({ message: "Categoria não encontrada" });
+      }
+    } catch {
+      res.status(500).json({ message: "Erro ao atualizar a categoria!!" });
+    }
+  }
+}
+
+export default new UpdatePostCategoriesController();
