b65561262138a27c93b74263ff5e91b87aa19d1b
Kyoudan <fogoste@gmail.com>
2023-03-31 09:30:48 -0300

    add: route to remove featured post

---
+++ src/controllers/FeaturedPosts/DeleteFeaturedPosts.controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import prismaClient from "../../database/prismaClient";
+
+class DeleteFeaturedPostsController {
+  async handle(req: Request, res: Response) {
+    try {
+      const queryId = req.params.id;
+
+      if (!queryId) return res.status(400).json({ message: "Id invalido!!" });
+      const id = parseInt(queryId);
+      if (isNaN(id)) return res.status(400).json({ message: "Id is NaN" });
+
+      await prismaClient.featuredPosts.delete({
+        where: {
+          id,
+        },
+      });
+
+      res
+        .status(200)
+        .json({ message: "Postagem em destaque retirada com sucesso!!" });
+    } catch {
+      res
+        .status(500)
+        .json({ message: "Erro ao apagar a postagem em destaque!!" });
+    }
+  }
+}
+
+export default new DeleteFeaturedPostsController();
