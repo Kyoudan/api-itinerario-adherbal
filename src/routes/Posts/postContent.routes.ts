fe25ddeb0ae69191a8e2e676f61cd69c0e656894
Kyoudan <fogoste@gmail.com>
2023-03-31 09:38:09 -0300

    add: linking routes from "postContent"

--- src/routes/Posts/postContent.routes.ts
+++ src/routes/Posts/postContent.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
 import CreatePostContentController from "../../controllers/Posts/PostContent/CreatePostContent.controller";
+import FindOnePostContentController from "../../controllers/Posts/PostContent/FindOnePostContent.controller";
+import UpdatePostContentController from "../../controllers/Posts/PostContent/UpdatePostContent.controller";
+import DeletePostContentController from "../../controllers/Posts/PostContent/DeletePostContent.controller";
 const routes = Router();
 
-routes.post("/postcontent", CreatePostContentController.handle)
+routes.get("/postcontent/:id", FindOnePostContentController.handle);
+routes.post("/postcontent", CreatePostContentController.handle);
+routes.put("/postcontent/:id", UpdatePostContentController.handle);
+routes.delete("/postcontent/:id", DeletePostContentController.handle);
 
 export default routes;
