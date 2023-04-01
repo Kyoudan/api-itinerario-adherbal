cf5be11e28b949849bf660bffce4512487351fec
Kyoudan <fogoste@gmail.com>
2023-04-01 19:48:09 -0300

    add: route to fetch all administrators

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -3,8 +3,10 @@ import LoginAdminController from "../controllers/Login/LoginAdmin.controller";
 import { Router } from "express";
 import UpdateAdminController from "@/controllers/Admin/UpdateAdmin.controller";
 import DeleteAdminController from "@/controllers/Admin/DeleteAdmin.controller";
+import FindAllAdminController from "@/controllers/Admin/FindAllAdmin.controller";
 const routes = Router();
 
+routes.get("/admins", FindAllAdminController.handle)
 routes.post("/loginadmin", LoginAdminController.handle);
 routes.post("/admin", CreateAdminController.handle);
 routes.put("/admin", UpdateAdminController.handle);
