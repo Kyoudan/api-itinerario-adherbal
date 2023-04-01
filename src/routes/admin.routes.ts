2ce9faa36ac6d13d4df64ab9675e0a75ffd58b74
Kyoudan <fogoste@gmail.com>
2023-04-01 19:49:34 -0300

    add: rota para buscar um administrador

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -4,9 +4,11 @@ import { Router } from "express";
 import UpdateAdminController from "@/controllers/Admin/UpdateAdmin.controller";
 import DeleteAdminController from "@/controllers/Admin/DeleteAdmin.controller";
 import FindAllAdminController from "@/controllers/Admin/FindAllAdmin.controller";
+import FindOneAdminController from "@/controllers/Admin/FindOneAdmin.controller";
 const routes = Router();
 
 routes.get("/admins", FindAllAdminController.handle)
+routes.get("/admins/:id", FindOneAdminController.handle)
 routes.post("/loginadmin", LoginAdminController.handle);
 routes.post("/admin", CreateAdminController.handle);
 routes.put("/admin", UpdateAdminController.handle);
