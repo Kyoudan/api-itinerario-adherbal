ef1fdc05a392e3410c2c7ef5522321972fde582f
Kyoudan <fogoste@gmail.com>
2023-04-01 19:40:29 -0300

    add: route to update an admin

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,9 +1,11 @@
 import CreateAdminController from "@/controllers/Admin/CreateAdmin.controller";
 import LoginAdminController from "../controllers/Login/LoginAdmin.controller";
 import { Router } from "express";
+import UpdateAdminController from "@/controllers/Admin/UpdateAdmin.controller";
 const routes = Router();
 
 routes.post("/loginadmin", LoginAdminController.handle);
 routes.post("/admin", CreateAdminController.handle);
+routes.put("/admin", UpdateAdminController.handle);
 
 export default routes;
