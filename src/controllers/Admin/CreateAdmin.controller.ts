a8270bd7dbe51f0251959645f24f48e14968040e
Kyoudan <fogoste@gmail.com>
2023-04-01 19:35:27 -0300

    add: route to create a new admin

---
+++ src/controllers/Admin/CreateAdmin.controller.ts
@@ -0,0 +1,38 @@
+import prismaClient from "@/database/prismaClient";
+import { UserType } from "@/types/UserType";
+import { Response } from "express";
+import { hashSync } from "bcrypt";
+
+class CreateAdminController {
+  async handle(req: UserType, res: Response) {
+    try {
+      const { name, email, password } = req.body;
+      const user = req.user;
+
+      if (!name) return res.status(400).json({ message: "Nome invalido!!" });
+      if (!email) return res.status(400).json({ message: "Email invalido!!" });
+      if (!user) return res.status(400).json({ message: "Adm não encontrado" });
+      if (!password)
+        return res.status(400).json({ message: "Senha invalida!!" });
+
+      const hash = hashSync(password, 8);
+      const lowerEmail = email.toLowerCase();
+
+      await prismaClient.admins.create({
+        data: {
+          name,
+          email: lowerEmail,
+          password: hash,
+        },
+      });
+
+      res
+        .status(201)
+        .json({ message: "Administrador cadastrado com sucesso!!" });
+    } catch {
+      return res.status(500).json({ message: "Erro ao criar administrador!!" });
+    }
+  }
+}
+
+export default new CreateAdminController();
--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,7 +1,9 @@
+import CreateAdminController from "@/controllers/Admin/CreateAdmin.controller";
 import LoginAdminController from "../controllers/Login/LoginAdmin.controller";
 import { Router } from "express";
 const routes = Router();
 
 routes.post("/loginadmin", LoginAdminController.handle);
+routes.post("/admin", CreateAdminController.handle);
 
 export default routes;
