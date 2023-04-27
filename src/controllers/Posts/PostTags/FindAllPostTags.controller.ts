c77cc4af65d46b6e90f738055f82d1130e449404
Kyoudan <fogoste@gmail.com>
2023-04-27 11:39:33 -0300

    update: increasing the number of queries to be received in the 'findAllPostsTags' route

--- src/controllers/Posts/PostTags/FindAllPostTags.controller.ts
+++ src/controllers/Posts/PostTags/FindAllPostTags.controller.ts
@@ -1,29 +1,49 @@
 import { Response } from "express";
 import { Request } from "express";
 import prismaClient from "../../../database/prismaClient";
+import { Sql } from "@prisma/client/runtime";
 
 class FindAllPostCategoriesController {
   async handle(req: Request, res: Response) {
-    let queryInit = req.query.init as string;
-    let queryLimit = req.query.limit as string;
-
-    !queryInit ? (queryInit = "0") : undefined;
-    !queryLimit ? (queryLimit = "100000") : undefined;
-
-    const init = parseInt(queryInit);
-    const limit = parseInt(queryLimit);
-
     try {
+      let queryInit = req.query.init as string;
+      let queryLimit = req.query.limit as string;
+      let findQuery = req.query.find as string;
+
+      !queryInit ? (queryInit = "0") : undefined;
+      !queryLimit ? (queryLimit = "100000") : undefined;
+
+      const init = parseInt(queryInit);
+      const limit = parseInt(queryLimit);
+      const find = parseInt(findQuery);
+
+      let data = {};
+
       const count = await prismaClient.postTags.count();
-      const data = await prismaClient.postTags.findMany({
-        skip: init,
-        take: limit,
-        select: {
-          id: true,
-          name: true,
-          createdAt: true,
-        },
-      });
+
+      if (findQuery && isNaN(find)) {
+        data =
+          await prismaClient.$queryRaw`SELECT * FROM posttags WHERE lower(name) LIKE lower(${find})`.count();
+      } else if (findQuery && !isNaN(find)) {
+        const result = await prismaClient.postTags.findMany({
+          where: {
+            id: find,
+          },
+        });
+        if (result) {
+          data = result;
+        }
+      } else {
+        data = await prismaClient.postTags.findMany({
+          skip: init,
+          take: limit,
+          select: {
+            id: true,
+            name: true,
+            createdAt: true,
+          },
+        });
+      }
       res.status(200).json({ data, count });
     } catch {
       res.status(500).json({ message: "Erro ao buscar os dados" });
--- src/routes/Posts/postTags.routes.ts
+++ src/routes/Posts/postTags.routes.ts
@@ -4,12 +4,13 @@ import DeletePostTagsController from "../../controllers/Posts/PostTags/DeletePos
 import FindAllPostTagsController from "../../controllers/Posts/PostTags/FindAllPostTags.controller";
 import FindOnePostTagsController from "../../controllers/Posts/PostTags/FindOnePostTags.controller";
 import UpdatePostTagsController from "../../controllers/Posts/PostTags/UpdatePostTags.controller";
+import AuthAdmin from "../..//middleware/AuthAdmin";
 const routes = Router();
 
-routes.get("/postTags", FindAllPostTagsController.handle);
-routes.get("/postTags/:id", FindOnePostTagsController.handle);
-routes.post("/postTags", CreatePostTagsController.handle);
-routes.put("/postTags/:id", UpdatePostTagsController.handle);
-routes.delete("/postTags/:id", DeletePostTagsController.handle);
+routes.get("/postTags", AuthAdmin, FindAllPostTagsController.handle);
+routes.get("/postTags/:id", AuthAdmin, FindOnePostTagsController.handle);
+routes.post("/postTags", AuthAdmin, CreatePostTagsController.handle);
+routes.put("/postTags/:id", AuthAdmin, UpdatePostTagsController.handle);
+routes.delete("/postTags/:id", AuthAdmin, DeletePostTagsController.handle);
 
 export default routes;
