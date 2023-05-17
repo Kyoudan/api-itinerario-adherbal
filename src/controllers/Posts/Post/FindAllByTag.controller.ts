f4da7d62f05d432058533361a8e04dbe5e484ce3
Martins-pedro23 <martinsdeoliveira.pedro2@gmail.com>
2023-05-16 22:12:06 -0300

    add: adição pesquisa por nome

--- src/controllers/Posts/Post/FindAllByTag.controller.ts
+++ src/controllers/Posts/Post/FindAllByTag.controller.ts
@@ -5,7 +5,8 @@ export const FindAllByTagController = async (req: Request, res: Response) => {
   try {
     let queryInit = req.query.init as string;
     let queryLimit = req.query.limit as string;
-    let queryTag = req.query.tag as string;
+    const queryTag = req.query.tag as string;
+    const queryFind = req.query.find as string;
 
     if (!queryTag)
       return res.status(400).json({ message: "Categoria invalida!!" });
@@ -18,19 +19,36 @@ export const FindAllByTagController = async (req: Request, res: Response) => {
 
     const count = await prismaClient.posts.count();
 
-    const data = await prismaClient.posts.findMany({
-      skip: init,
-      take: limit,
-      where: {
-        postTags: {
-          name: {
-            contains: queryTag,
+    
+    if (queryFind) {
+      const find = queryFind.replace(/ /g, "-").toLowerCase( )
+      const data = await prismaClient.posts.findMany({
+        skip: init,
+        take: limit,
+        where: {
+          slug: find,
+          postTags: {
+            name: {
+              contains: queryTag,
+            },
           },
         },
-      },
-    });
-
-    res.status(200).json({ data, count });
+      });
+      res.status(200).json({ data, count });
+    } else {
+      const data = await prismaClient.posts.findMany({
+        skip: init,
+        take: limit,
+        where: {
+          postTags: {
+            name: {
+              contains: queryTag,
+            },
+          },
+        },
+      });
+      res.status(200).json({ data, count });
+    }
   } catch {
     res.status(500).json({ message: "Erro no servidor!!" });
   }
