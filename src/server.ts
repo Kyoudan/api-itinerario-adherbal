d471c48fb43916b2c5b25a29f8705c4190706f7b
Kyoudan <fogoste@gmail.com>
2023-06-12 12:06:12 -0300

    add: style console.log in app.listen

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,34 @@
 import { config } from "dotenv";
 import { app, boostrapStart } from "./app";
+import { green, cyan, magenta } from "colorette";
 config();
 
 boostrapStart();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`🔥 Back-end rodando na porta: http://localhost:${port}`);
-  console.log(`💦 Acesse a documentação em:  http://localhost:${port}/api`);
-  console.log(`💫 Acesse o Apollo server em:  http://localhost:${port}/graphql`);
+const server = app.listen(port, () => {
+  console.log("-------------------------------------------------------------");
+  console.log(" ");
+  console.log(
+    `🔥 Back-end rodando na porta: ${green(`http://localhost:${port}`)}`
+  );
+  console.log(
+    `💦 Acesse a documentação em:  ${cyan(`http://localhost:${port}/api`)}`
+  );
+  console.log(
+    `💫 Acesse o Apollo server em:  ${magenta(
+      `http://localhost:${port}/graphql`
+    )}`
+  );
+  console.log(" ");
+  const address = server.address();
+  if (address && typeof address !== "string") {
+    const { address: ip, port } = address;
+    if (ip.length > 4) {
+      console.log(`Server running at http://${ip}:${port}/`);
+      console.log(" ");
+    }
+  }
+  console.log("-------------------------------------------------------------");
 });
