1fafc13c8ff94b667ecce361940a3ff745d46054
Kyoudan <fogoste@gmail.com>
2023-06-13 09:06:16 -0300

    config: health route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import PostRoutes from "./Posts/post.routes";
 import PostContentRoutes from "./Posts/postContent.routes";
 import featuredPostsRoutes from "./Posts/featuredPost.routes";
 import adminRoutes from "./admin.routes";
+import prismaClient from "@/database/prismaClient";
 const routes = Router();
 
 routes.use(PostTagsRoutes);
@@ -12,8 +13,22 @@ routes.use(PostContentRoutes);
 routes.use(featuredPostsRoutes);
 routes.use(adminRoutes);
 
-routes.get("/health", (req, res) => {
-  res.status(200).json({ message: "API OK!" });
+routes.get("/health", async (req, res) => {
+  try {
+    const result = await prismaClient.$connect();
+    res.status(200).json({
+      message: "Api e banco de dados conectados",
+      status: "OK",
+      db: result,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(200).json({
+      message: "ERROR",
+      status: "ERROR",
+      db: err,
+    });
+  }
 });
 
 export default routes;
